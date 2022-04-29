006abbda08d8ee9f54bf9c0211fdb74a40c27003
Eugene Shmakov <schmak.ekb@gmail.com>
2022-04-29 20:25:07 +0500

    #1. Added navigation components

---
+++ app/src/main/kotlin/com/mss/app/navigation/NavGraph.kt
@@ -0,0 +1,29 @@
+package com.mss.app.navigation
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.mss.app.R
+import com.mss.app.navigation.Route.Companion.composable
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    startDestination: Route = Route.Series,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination.value,
+        modifier = modifier
+    ) {
+        composable(Route.Series) { Text(stringResource(R.string.series)) }
+        composable(Route.Results) { Text(stringResource(R.string.results)) }
+        composable(Route.Drivers) { Text(stringResource(R.string.drivers)) }
+        composable(Route.Teams) { Text(stringResource(R.string.teams)) }
+        composable(Route.Venues) { Text(stringResource(R.string.venues)) }
+    }
+}
