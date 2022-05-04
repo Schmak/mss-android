c57aa02f35115cd8fde796ca775af62650485851
Eugene Shmakov <schmak.ekb@gmail.com>
2022-05-04 18:46:11 +0500

    #3. Added SlopeSideContainer

---
+++ app/src/main/kotlin/com/mss/app/ui/theme/shapes/SlopeSideShape.kt
@@ -0,0 +1,35 @@
+package com.mss.app.ui.theme.shapes
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.mss.app.ui.theme.shapes.SlopeSideShape.CornerType.Bottom
+import com.mss.app.ui.theme.shapes.SlopeSideShape.CornerType.Top
+
+class SlopeSideShape(
+    private val leftCornerType: CornerType = Top,
+    private val rightCornerType: CornerType = Top,
+    private val leftCornerWidth: Dp = 0.dp,
+    private val rightCornerWidth: Dp = 0.dp,
+) : Shape {
+    override fun createOutline(size: Size, layoutDirection: LayoutDirection, density: Density): Outline =
+        with(density) {
+            Path().apply {
+                moveTo(if (leftCornerType == Top) leftCornerWidth.toPx() else 0f, 0f)
+                lineTo(size.width - if (rightCornerType == Top) rightCornerWidth.toPx() else 0f, 0f)
+                lineTo(size.width - if (rightCornerType == Bottom) rightCornerWidth.toPx() else 0f, size.height)
+                lineTo(if (leftCornerType == Bottom) leftCornerWidth.toPx() else 0f, size.height)
+                close()
+            }.let(Outline::Generic)
+        }
+
+    enum class CornerType {
+        Top,
+        Bottom,
+    }
+}
