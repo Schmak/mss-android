6fb0406d6f77ba2940c66b1897182005b5499b1c
Eugene Shmakov <schmak.ekb@gmail.com>
2022-05-20 20:26:55 +0500

    #22. Moved core-network-model to core/network-model

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -3,7 +3,7 @@
 object Projects {
     object Core {
         const val NETWORK = ":core:network"
-        const val NETWORK_MODEL = ":core-network-model"
+        const val NETWORK_MODEL = ":core:network-model"
         const val NETWORK_TEST_HELPERS = ":core-network-test-helpers"
         const val TEST_HELPERS = ":core-test-helpers"
         const val UI = ":core-ui"
