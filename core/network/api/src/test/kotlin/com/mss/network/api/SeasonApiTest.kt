67218e355e10f22625f07f29172581bff36268d8
Eugene Shmakov <schmak.ekb@gmail.com>
2022-05-22 10:10:54 +0500

    #25. Extracted common *ApiTest code to AbstractApiTest

--- core/network/api/src/test/kotlin/com/mss/network/api/SeasonApiTest.kt
+++ core/network/api/src/test/kotlin/com/mss/network/api/SeasonApiTest.kt
@@ -1,19 +1,11 @@
 package com.mss.network.api
 
-import com.mss.annotation.IntegrationTest
 import com.mss.network.di.ApiModule
-import com.mss.network.model.PageDto
 import com.mss.network.model.sort.OrderByDto.Companion.asc
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
 import utils.testRetrofit
 
-@ExperimentalCoroutinesApi
-@IntegrationTest
-internal class SeasonApiTest {
+internal class SeasonApiTest : AbstractApiTest() {
     private val api = ApiModule.provideSeasonApi(testRetrofit)
 
     @Test
@@ -25,13 +17,5 @@ internal class SeasonApiTest {
             size = 10,
             orderBy = SeasonApi.TeamOrder.ChampionshipPosition.asc,
         )
-    }
-
-    private fun test(apiCall: suspend () -> Any) = runTest {
-        when (val actual = assertDoesNotThrow { apiCall() }) {
-            is PageDto<*> -> assertThat(actual.content).isNotEmpty
-            is List<*> -> assertThat(actual).isNotEmpty
-            else -> assertThat(actual).isNotNull
-        }
     }
 }
--- core/network/api/src/test/kotlin/com/mss/network/api/TeamApiTest.kt
+++ core/network/api/src/test/kotlin/com/mss/network/api/TeamApiTest.kt
@@ -1,18 +1,10 @@
 package com.mss.network.api
 
-import com.mss.annotation.IntegrationTest
 import com.mss.network.di.ApiModule
-import com.mss.network.model.PageDto
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
 import utils.testRetrofit
 
-@ExperimentalCoroutinesApi
-@IntegrationTest
-internal class TeamApiTest {
+internal class TeamApiTest : AbstractApiTest() {
     private val api = ApiModule.provideTeamApi(testRetrofit)
 
     @Test
@@ -31,13 +23,5 @@ internal class TeamApiTest {
             page = 0,
             size = 10,
         )
-    }
-
-    private fun test(apiCall: suspend () -> Any) = runTest {
-        when (val actual = assertDoesNotThrow { apiCall() }) {
-            is PageDto<*> -> assertThat(actual.content).isNotEmpty
-            is List<*> -> assertThat(actual).isNotEmpty
-            else -> assertThat(actual).isNotNull
-        }
     }
 }
