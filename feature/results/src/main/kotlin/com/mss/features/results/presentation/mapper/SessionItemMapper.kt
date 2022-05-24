fcb78df1cf58392379b6b3c97fea6403fb9b9760
Eugene Shmakov <schmak.ekb@gmail.com>
2022-05-24 18:31:15 +0500

    #30. Format dates according to selected Locale

--- feature/results/src/main/kotlin/com/mss/features/results/presentation/mapper/SessionItemMapper.kt
+++ feature/results/src/main/kotlin/com/mss/features/results/presentation/mapper/SessionItemMapper.kt
@@ -2,6 +2,7 @@ package com.mss.features.results.presentation.mapper
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import com.mss.core.domain.SessionItem
 import com.mss.core.ui.model.UiItem
@@ -9,6 +10,7 @@ import com.mss.core.utils.Mapper
 import com.mss.features.results.R
 import com.mss.features.results.presentation.ui.landing.state.SessionUiItem
 import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 sealed class SessionItemMapper(
     @StringRes private val dateSubtitleId: Int,
@@ -18,7 +20,11 @@ sealed class SessionItemMapper(
     override fun SessionItem.map(): SessionUiItem {
         val nameLambda = @Composable { name }
         val dateLambda = @Composable {
-            stringResource(dateSubtitleId, startTime?.let(::convert).orEmpty())
+            val pattern = stringResource(R.string.time_format)
+            val formattedTime = remember(pattern) {
+                startTime?.format(DateTimeFormatter.ofPattern(pattern)).orEmpty()
+            }
+            stringResource(dateSubtitleId, formattedTime)
         }
         return SessionUiItem(
             imageUrl = series.picture.orEmpty(),
