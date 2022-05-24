cf6b136c38fe1d4a31757f9d79a3f274c5723b38
Eugene Shmakov <schmak.ekb@gmail.com>
2022-05-24 18:44:55 +0500

    #30. Convert time between venue and client time

--- feature/results/src/main/kotlin/com/mss/features/results/presentation/mapper/SessionItemMapper.kt
+++ feature/results/src/main/kotlin/com/mss/features/results/presentation/mapper/SessionItemMapper.kt
@@ -9,20 +9,25 @@ import com.mss.core.ui.model.UiItem
 import com.mss.core.utils.Mapper
 import com.mss.features.results.R
 import com.mss.features.results.presentation.ui.landing.state.SessionUiItem
+import java.time.LocalDateTime
 import java.time.OffsetDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 sealed class SessionItemMapper(
     @StringRes private val dateSubtitleId: Int,
 ) : Mapper<SessionItem, UiItem> {
-    protected abstract fun convert(time: OffsetDateTime): String
+    protected abstract fun convert(time: OffsetDateTime): LocalDateTime
 
     override fun SessionItem.map(): SessionUiItem {
         val nameLambda = @Composable { name }
         val dateLambda = @Composable {
             val pattern = stringResource(R.string.time_format)
-            val formattedTime = remember(pattern) {
-                startTime?.format(DateTimeFormatter.ofPattern(pattern)).orEmpty()
+            val formattedTime = remember(pattern, this@SessionItemMapper) {
+                startTime
+                    ?.let(::convert)
+                    ?.format(DateTimeFormatter.ofPattern(pattern))
+                    .orEmpty()
             }
             stringResource(dateSubtitleId, formattedTime)
         }
@@ -34,10 +39,13 @@ sealed class SessionItemMapper(
     }
 
     object VenueTime : SessionItemMapper(R.string.venue_time) {
-        override fun convert(time: OffsetDateTime): String = time.toString()
+        override fun convert(time: OffsetDateTime): LocalDateTime = time.toLocalDateTime()
     }
 
     object ClientTime : SessionItemMapper(R.string.your_time) {
-        override fun convert(time: OffsetDateTime): String = time.toString()
+        override fun convert(time: OffsetDateTime): LocalDateTime =
+            time.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime()
     }
 }
